d9a278e69da1625bb78e7ca15a77264c3f02bb33
YU cheng <s878072005@gmail.com>
2020-08-10 14:04:41 +0800

    cock

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,5 @@ int main()
 	std::cout << "hello hell" << std::endl;
 	std::cout << "hello meh" << std::endl;
         std::cout << "hello lemon" << std::endl;
+        std::cout << "hello cock" << std::endl;
 }	
